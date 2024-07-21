4ab65a151d519674a0f621e2dd71e2d40edeffc7
keesjansma5000 <keesjansma5000@gmail.com>
2024-07-21 14:26:00 +0200

    .

--- next.config.mjs
+++ next.config.mjs
@@ -4,18 +4,7 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  webpack: (config, { dev, isServer }) => {
-    // Optimize only for production builds
-    if (!dev && !isServer) {
-      Object.assign(config.resolve.alias, {
-        'react/jsx-runtime.js': 'preact/compat/jsx-runtime',
-        react: 'preact/compat',
-        'react-dom/test-utils': 'preact/test-utils',
-        'react-dom': 'preact/compat',
-      });
-    }
-    return config;
-  },
+  // Remove the webpack configuration that replaces React with Preact
 };
 
 export default nextConfig;
